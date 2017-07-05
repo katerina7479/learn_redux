f6465d6b4ea191769a76f1c9f680bef7e5e376ab
Katerina Hanson <katerina.hanson@gmail.com>
2017-07-04 17:33:43 -0700

    Combine reducers

--- app/redux-example.jsx
+++ app/redux-example.jsx
@@ -4,33 +4,52 @@ console.log('Starting Redux example');
 
 var stateDefault = {
   name: 'Anonymous',
-  hobbies: []
+  hobbies: [],
+  movies: []
 }
 
-var reducer = (state = stateDefault, action) => {
+var nameReducer = (state = 'Anonymous', action) => {
   switch (action.type) {
     case 'CHANGE_NAME':
-      return {
-        ...state,
-        name: action.name
-      }
-    case 'ADD_HOBBY':
-      return {
-        ...state,
-        hobbies: [
-          ...state.hobbies,
-          action.hobby
-        ]
-      }
-    case 'REMOVE_HOBBY':
-      return {
-        ...state,
-        hobbies: state.hobbies.filter((hobby) => hobby != action.hobby)
-      }
+      return action.name
     default:
-      return state;
+      return state
   }
-};
+}
+
+var hobbiesReducer = (state = [], action) => {
+  switch (action.type) {
+    case 'ADD_HOBBY':
+      return [
+        ...state,
+        action.hobby
+      ]
+    case 'REMOVE_HOBBY':
+      return state.filter((hobby) => hobby == action.hobby)
+    default:
+      return state
+  }
+}
+
+var moviesReducer = (state = [], action) => {
+  switch (action.type) {
+    case 'ADD_MOVIE':
+      return [
+        ...state,
+        action.movie
+      ]
+    case 'REMOVE_MOVIE':
+      return state.filter((movie) => movie == action.movie)
+    default:
+      return state
+  }
+}
+
+var reducer = Redux.combineReducers({
+  name: nameReducer,
+  hobbies: hobbiesReducer,
+  movies: moviesReducer
+});
 
 var store = Redux.createStore(reducer, Redux.compose(
   window.devToolsExtension ? window.devToolsExtension() : f => f
@@ -67,4 +86,8 @@ store.dispatch({
 store.dispatch({
   type: 'REMOVE_HOBBY',
   hobby: 'Running'
+})
+store.dispatch({
+  type: 'ADD_MOVIE',
+  movie: 'Finding Nemo'
 })
